6d883af93a3c305654ba7c0f9359486baf1e716c
Abdullah <callmeab26@gmail.com>
2026-07-09 19:31:54 +0500

    chore: update wrangler build artifacts and middleware configuration

--- .wrangler/tmp/bundle-8zWOCl/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-8zWOCl/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-oUA0aL\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-8zWOCl\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\working\\Angular\\phonedeals-uk\\worker\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-oUA0aL\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-8zWOCl\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-oUA0aL\\middleware-insertion-facade.js";
+export * from "C:\\working\\Angular\\phonedeals-uk\\.wrangler\\tmp\\bundle-8zWOCl\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
